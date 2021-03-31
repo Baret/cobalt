0b288179c667c72e2c1ff32f18a62f46fec57e38
Baret <450955+Baret@users.noreply.github.com>
2021-03-31 09:32:00 +0200

    Fixed unittest

--- cobalt.core/src/jvmTest/kotlin/org/hexworks/cobalt/databinding/internal/binding/MaybeBindingTest.kt
+++ cobalt.core/src/jvmTest/kotlin/org/hexworks/cobalt/databinding/internal/binding/MaybeBindingTest.kt
@@ -1,9 +1,8 @@
 package org.hexworks.cobalt.databinding.internal.binding
 
-import org.hexworks.cobalt.databinding.api.binding.bindOrElse
-import org.hexworks.cobalt.databinding.api.binding.bindOrElseGet
-import org.hexworks.cobalt.databinding.api.binding.bindTransformOrElse
-import org.hexworks.cobalt.databinding.api.binding.bindTransformOrElseGet
+import org.hexworks.cobalt.core.internal.Atom
+import org.hexworks.cobalt.core.internal.toAtom
+import org.hexworks.cobalt.databinding.api.binding.*
 import org.hexworks.cobalt.databinding.api.extension.createPropertyFrom
 import org.hexworks.cobalt.databinding.api.property.Property
 import org.hexworks.cobalt.databinding.api.value.ObservableValue
@@ -12,14 +11,14 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class MaybeBindingTest {
-
+    
     private val prop: Property<Maybe<Foo>> = createPropertyFrom(Maybe.of(Foo()))
 
     private val unitUnderTest: ObservableValue<Maybe<Foo>> = prop
 
     @Test
     fun test_BindOrElse() {
-        val binding = unitUnderTest.bindOrElse(Foo(16))
+        val binding: Binding<Foo> = unitUnderTest.bindOrElse(Foo(16))
 
         prop.updateValue(Maybe.empty())
 
@@ -38,17 +37,26 @@ class MaybeBindingTest {
 
     @Test
     fun test_BindOrElseGet() {
-        var changedCounter = 0
-        val binding = unitUnderTest.bindOrElseGet {
+        var changedCounter: Atom<Int> = 0.toAtom()
+        val binding: Binding<Foo> = unitUnderTest.bindOrElseGet {
             changedCounter++
             Foo(22)
         }
 
-        assertEquals(0, changedCounter)
+        assertEquals(
+            expected = 0,
+            actual = changedCounter.get(),
+            message = "Fetched default after binding"
+        )
 
         prop.updateValue(Maybe.empty())
 
-        assertEquals(1, changedCounter)
+        assertEquals(
+            expected = 1,
+            actual = changedCounter.get(),
+            message = "Fetched default after setting to empty Maybe"
+        )
+
         assertEquals(
             expected = Foo(22),
             actual = binding.value
@@ -56,16 +64,34 @@ class MaybeBindingTest {
 
         prop.updateValue(Maybe.of(Foo(-3)))
 
-        assertEquals(1, changedCounter)
+        // We expect 2 calls to the default getter here
+        // because on every updateValue() the old value is also transformed one more time
+        assertEquals(
+            expected = 2,
+            actual = changedCounter.get(),
+            message = "Fetched default after setting value (NO more default!)"
+        )
         assertEquals(
             expected = Foo(-3),
+            actual = binding.value
+        )
+
+        prop.updateValue(Maybe.of(Foo(100)))
+
+        assertEquals(
+            expected = 2,
+            actual = changedCounter.get(),
+            message = "Fetched default after setting value again (NO more default!)"
+        )
+        assertEquals(
+            expected = Foo(100),
             actual = binding.value
         )
     }
 
     @Test
     fun test_BindTransformOrElse() {
-        val binding = unitUnderTest.bindTransformOrElse(17) { it.bar }
+        val binding: Binding<Int> = unitUnderTest.bindTransformOrElse(17) { it.bar }
 
         prop.updateValue(Maybe.empty())
 
@@ -84,28 +110,51 @@ class MaybeBindingTest {
 
     @Test
     fun test_BindTransformOrElseGet() {
-        var changedCounter = 0
-        val binding = unitUnderTest.bindTransformOrElseGet({
+        var changedCounter: Atom<Int> = 0.toAtom()
+        val binding: Binding<Int> = unitUnderTest.bindTransformOrElseGet({
             changedCounter++
             99
         }) { it.bar }
 
         prop.updateValue(Maybe.empty())
 
-        assertEquals(1, changedCounter)
         assertEquals(
-            expected = 17,
+            expected = 1,
+            actual = changedCounter.get()
+        )
+        assertEquals(
+            expected = 99,
             actual = binding.value
         )
 
         prop.updateValue(Maybe.of(Foo(1337)))
 
-        assertEquals(1, changedCounter)
+        // We expect 2 calls to the default getter here
+        // because on every updateValue() the old value is also transformed one more time
+        assertEquals(
+            expected = 2,
+            actual = changedCounter.get()
+        )
         assertEquals(
             expected = 1337,
+            actual = binding.value
+        )
+
+        prop.updateValue(Maybe.of(Foo(1)))
+
+        assertEquals(
+            expected = 2,
+            actual = changedCounter.get()
+        )
+        assertEquals(
+            expected = 1,
             actual = binding.value
         )
     }
 }
 
-private data class Foo(val bar: Int = 5)
+private operator fun Atom<Int>.inc(): Atom<Int> = also {
+    transform { it + 1 }
+}
+
+private data class Foo(val bar: Int = 0)
